b739815d7fe85a94a25f0945d5d3e82e5bec7983
Muntasir Ahmed <muntasir.ahmed@enosisbd.com>
2017-02-20 19:10:16 +0600

    Error Message regex path and constraint

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const Relish = function Relish (opts) {
 
   this.parseError = (error) => {
     return error.data.details.map((i) => {
+
       let err = {
         key: i.path.split('.').pop(),
         path: i.path,
@@ -22,7 +23,6 @@ const Relish = function Relish (opts) {
         type: i.type.split('.').shift(),
         constraint: i.type.split('.').pop()
       }
-
       // if label is different than key, provide label
       if (i.context.key !== err.key) {
         err.label = i.context.key
@@ -32,8 +32,28 @@ const Relish = function Relish (opts) {
       if (this._opts.messages.hasOwnProperty(err.path)) {
         err.message = this._opts.messages[err.path]
       } else if (this._opts.messages.hasOwnProperty(err.key)) {
-        err.message = this._opts.messages[err.key]
+        err.message = this._opts.messages[err.key];
+      } else {
+        //loop through the messages object to math path using regex
+        for (var relishPath in this._opts.messages) {
+          if (this._opts.messages.hasOwnProperty(relishPath)) {
+
+            this._opts.regexObj = this._opts.regexObj || {};
+
+            //Adds ^ at start and $ at the end to match exact path and make regex as path
+            let newRelishPath = this._opts.regexObj[relishPath] || this.addRegex(relishPath);
+
+            //if path matches with object's property
+            if (err.path.match(newRelishPath)) {
+              err.message = this._opts.messages[relishPath];
+            }
+          }
+        }
       }
+      //Overwrite error message with constraint specific error messsage
+      err.message = err.message[err.constraint] || err.message['default'] || err.message;
+      //Adding interaction number if present
+      err.path = this.addPath(err.path, error);
 
       return err
     })
@@ -57,16 +77,48 @@ const Relish = function Relish (opts) {
   }
 
   this.exports.failAction = (request, reply, source, error) => {
+
     // parse error object
-    const errors = this.parseError(error)
+    const errors = this.parseError(error);
 
     // build main error message
-    const errorMessage = errors.map((e) => e.message).join(', ')
+    const errorMessage = errors.map((e) => JSON.stringify(e.message)).join(', ');
 
     // format error response
-    error = this.formatResponse(error, source, errorMessage, errors)
+    error = this.formatResponse(error, source, errorMessage, errors);
 
     return reply(error)
+  }
+  /**
+   * Adds ^ before path and $ at the end if not present
+   * @param relishPath
+   * @returns {RegExp}
+   */
+  this.addRegex = (relishPath) => {
+
+    //Add ^ in beginning if not present
+    if (!relishPath.startsWith('^')) {
+      relishPath = '^' + relishPath;
+    }
+    //Add $ in end if not present
+    if (!relishPath.endsWith('$')) {
+      relishPath = relishPath + '$';
+    }
+
+    return new RegExp(relishPath);
+  }
+  /**
+   * Adds full path with the existing path
+   * @param errorPath
+   * @param error
+   * @returns errorPath
+   */
+  this.addPath = (errorPath, error) => {
+
+    if (error.data.path) {
+      errorPath = error.data.path + '.' + errorPath;
+    }
+    return errorPath;
   }
 
   return this.exports
